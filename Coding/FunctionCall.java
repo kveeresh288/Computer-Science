62e27a9cad6284dcca7ce03d83cbb5cdb5a01a38
K VEERESH <kveeresh288@gmail.com>
2025-07-24 00:25:59 +0530

    Day 13 Basic Functions Examples

---
+++ Coding/FunctionCall.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class FunctionCall {
+   public static void main(String[] var0) {
+    //Find a Number Even Or Odd
+    Scanner sc =new Scanner(System.in);
+    System.out.print("Enter a Number1 ");
+    int a = sc.nextInt();
+    System.out.print("Enter a Number2 ");
+    int b = sc.nextInt();
+    printSumOfTwo(a,b);
+
+   }
+  static void printSumOfTwo(int number1,int number2){
+    int sum = getSumOfTwo(number1,number2);
+    System.out.println("The Sum of Two NUmbers: "+ sum);
+
+  }
+  static int  getSumOfTwo(int num1,int num2){
+   return num1+num2;
+  }
+}
