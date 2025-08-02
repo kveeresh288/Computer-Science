87468b319222b4ab16ada5240ca0de16478cd773
K VEERESH <kveeresh288@gmail.com>
2025-08-02 23:06:46 +0530

    Day 23 functions Array String Binary Search

--- Coding/Java/Functions/Practice.java
+++ Coding/Java/Functions/Practice.java
@@ -1,5 +1,6 @@
 package Functions;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Practice {
@@ -88,8 +89,8 @@ public class Practice {
         }
         return c;
     }
-    
 
+   
     static String reveseString(String input){
     StringBuilder output = new StringBuilder();
 
@@ -132,6 +133,86 @@ public class Practice {
         System.out.println("Input " + input1 + " Output " + output1);
     }
 
+    public static int getArrSum(int[] arr){
+        int sum=0;
+        for(int i:arr)sum+=i;
+        return sum;
+    }
+    
+    public static void invoke_getArrSum(){
+        int arr [] = {2,4,5,6,7};
+        System.out.println("Array Elements [2,4,5,6,7] sum : "+ getArrSum(arr));
+    }
+
+    public static boolean isPalindrome(String s){
+        int st=0; int end = s.length()-1;
+        while (st<end){
+            if(s.charAt(st)!=s.charAt(end))return false;  
+            st++;
+            end--;
+        }
+       
+        return true;
+    }
+
+    public static void invoke_isPalindrome(){
+        System.out.println("String ''malayalam'' is palindrome : "+ isPalindrome("malayalam"));
+    }
+
+    public static void printMinMax(int[] arr){
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for(int i: arr){
+            if(max<i)max=i;
+            if(min>i)min=i;
+        }
+        System.out.println("Min value in array is "+ min );
+        System.out.println("Max value in array is "+ max );
+    }
+    public static void invoke_printMinMax(){
+        int arr[] = {3,5,7,-2,6,9,2};
+        System.out.println(Arrays.toString(arr));
+        printMinMax(arr);
+    }
+
+    public static int binarySearch(int[] arr,int key){
+        int st = 0 ;
+        int  end = arr.length-1; 
+        while (st<=end) { 
+            int mid = st+((end-st)/2);
+            if(arr[mid]==key)return mid;
+            if(arr[mid]>key)end=mid-1;
+            else st = mid+1;
+        }
+        return -1;
+    }
+
+    public static void invoke_binarySearch(){
+        int arr[]= {1,4,6,8,12,32,67,90};
+        System.out.println("Key 32 found at position "+ (binarySearch(arr, 32)+1));
+    }
+
+    public static int[] mergeArrays(int[] arr1, int[] arr2) {
+        int n1 = arr1.length;
+        int n2 = arr2.length;
+        int[] merged = new int[n1 + n2];
+        for (int i = 0; i < n1; i++) {
+            merged[i] = arr1[i];
+        }
+        for (int i = 0; i < n2; i++) {
+            merged[n1 + i] = arr2[i];
+        }
+        return merged;
+    }
+    public static void invoke_mergeArrays(){
+        int[] array1 = {1, 3, 5, 7};
+        int[] array2 = {2, 4, 6, 8};
+        System.out.println("arr1 : "+ Arrays.toString(array1));
+        System.out.println("arr2 : "+ Arrays.toString(array2));
+        int[] mergedArray = mergeArrays(array1, array2);
+        System.out.print("Merged Array: ");
+        System.out.println(Arrays.toString(mergedArray));
+    }
     public static void main(String[] args) {
         invoke_getSum();
         invoke_swap();
@@ -142,6 +223,11 @@ public class Practice {
         System.out.println("Get String Length of: ''Veeresh'' = "+ getStringLen("Veeresh"));
         System.out.println("Get Vowels Count in: 'Veeresh' : "+ getVowelsCount("Veeresh"));
         invoke_reverseString();
+        invoke_getArrSum();
+        invoke_isPalindrome();
+        invoke_printMinMax();
+        invoke_binarySearch();
+        invoke_mergeArrays();
     }
 }
 
